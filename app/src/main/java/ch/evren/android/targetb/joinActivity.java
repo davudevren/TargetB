c5f02fefe23741fd1deb86fcf4dd682fedd60a6f
davudevren <davud_evren@yahoo.de>
2015-05-11 18:47:49 +0200

    erster commit nach Anfang der Implementation

--- app/src/main/java/ch/evren/android/targetb/joinActivity.java
+++ app/src/main/java/ch/evren/android/targetb/joinActivity.java
@@ -1,26 +1,24 @@
 package ch.evren.android.targetb;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class joinActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_join);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_join, menu);
         return true;
     }
 
@@ -37,15 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void join(View view) {
-        Intent joinIntent = new Intent(this, joinActivity.class);
-        startActivity(joinIntent);
-    }
-
-    public void start(View view) {
-        Intent startIntent = new Intent(this, startActivity.class);
-        startActivity(startIntent);
     }
 }
